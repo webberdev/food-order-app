736576eff07327d258024193d1ba37e7406b8e2c
jimmyli <jimmyliweb@gmail.com>
2023-01-10 22:28:41 +1100

    added button to header 004

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 export default function Header() {
   return (
     <>
       <header className={styles.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="table full of food" />
