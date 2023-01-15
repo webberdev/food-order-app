bc18e9541ad42f5a4c0a84d5c6c4273bcae2eb3b
jimmyli <jimmyliweb@gmail.com>
2023-01-15 16:25:20 +1100

    added modal window

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -8,17 +9,19 @@ const Cart = (props) => {
       ))}
     </ul>
   );
+
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.actions}></div>
-      <button className={classes['button--alt']}>Close</button>
-      <button className={classes.button}>Order</button>
-    </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
   );
 };
 
