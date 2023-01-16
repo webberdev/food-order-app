7dc9f4fd984b56212fe13ddfcb8d4071ab97ca14
jimmyli <jimmyliweb@gmail.com>
2023-01-16 19:27:33 +1100

    added close cart function

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import styles from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-export default function Header() {
+export default function Header(props) {
   return (
     <>
       <header className={styles.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="table full of food" />
